4df4ad06042b5a3a165ae6d162e6bb673692d929
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-11-19 16:14:14 +0200

    working

--- app/app.js
+++ app/app.js
@@ -58,8 +58,10 @@ angular.module('code-tutorial', ['ngMaterial', 'ui.ace'])
         BOTTOM: 2,
         LEFT: 3
       };
+      RobotTutorial.orientation = orientationEnum;
       var orientation = orientationEnum.UP;
       var orientationChangedListener = false;
+      var positionChangedListener = false;
       function changeOrientation(newOrientation) {
         var direction = 0;
         if (newOrientation === 0 && orientation === 3) {
@@ -73,77 +75,55 @@ angular.module('code-tutorial', ['ngMaterial', 'ui.ace'])
         }
         orientation = newOrientation;
         if (orientationChangedListener) {
-          var orient;
-          for (var prop in orientationEnum) {
-            if (orientationEnum.hasOwnProperty(prop) &&
-              orientation === orientationEnum[prop]) {
-              orient = prop;
-              break;
-            }
-          }
-          orient =
-            RobotTutorial.cssRobotRotate[orient];
+          orient = RobotTutorial.cssRobotRotate[getOrientationString()];
           orientationChangedListener(RobotTutorial.genaralRobotClass +
             ' ' + orient,
-            size * currentCoords[0] + currentCoords[1],
-            function(elements) {
-              if (!direction) {
-                return;
-              }
-              elements = $(elements);
-              var oldDegree = elements.css("-webkit-transform") ||
-                elements.css("-moz-transform")    ||
-                elements.css("-ms-transform")     ||
-                elements.css("-o-transform")      ||
-                elements.css("transform");
-              oldDegree = parseInt(oldDegree.match(/-?[0-9]+deg/) ||
-                elements[0].style.transform.match(/-?[0-9]+deg/)) || 0;
-              if (direction > 0) {
-                var newDegree = oldDegree + 90;
-              } else {
-                newDegree = oldDegree - 90;
-              }
-              $({deg: oldDegree}).animate({deg: newDegree}, {
-                duration: RobotTutorial.stepMilisecs,
-                step: function(now) {
-                  elements.css({
-                    transform: 'rotate(' + now + 'deg)'
-                  });
-                }
-              });
-              // element.animate({
-              //     transform: 'rotate(' + newDegree + 'deg)'
-              //   },
-              //   {
-              //     duration: RobotTutorial.stepMilisecs,
-              //     step: function(now, fx) {
-              //       debugger;
-              //     }
-              //   }
-              // );
-            });
+            direction,
+            size * currentCoords[0] + currentCoords[1]);
         }
+      }
+      function getOrientationString() {
+        var orient;
+        for (var prop in orientationEnum) {
+          if (orientationEnum.hasOwnProperty(prop) &&
+            orientation === orientationEnum[prop]) {
+            orient = prop;
+            break;
+          }
+        }
+        return orient;
       }
       var actions = {
         makeStep: function() {
           var coords = getAccessibleTile("Cannot go out of the board");
           if (!coords) {
-            return coords;
+            return false;
           }
-          //TODO: animate
+          var newCell = getTileAt(coords[0], coords[1]);
+          if (newCell.apple) {
+            alert('Can\'t go to apple');
+            return false;
+          }
+          var finishAnimation;
           getTileAt(currentCoords[0], currentCoords[1]).switch(CellInfo.statusEnum.EMPTY);
-          getTileAt(coords[0], coords[1]).switch(CellInfo.statusEnum.ROBOT);
+          if (positionChangedListener) {
+            positionChangedListener(orientation, function(callback) {
+              finishAnimation = callback;
+            });
+          }
+          newCell.switch(CellInfo.statusEnum.ROBOT);
+          if (finishAnimation) {
+            finishAnimation();
+          }
           currentCoords = coords;
           return true;
         },
         turnRight: function() {
           changeOrientation((orientation + 1) % 4);
-          //TODO: animate
           return true;
         },
         turnLeft: function() {
           changeOrientation((orientation - 1 + 4) % 4);
-          //TODO: animate
           return true;
         },
         takeApple: function() {
@@ -199,25 +179,28 @@ angular.module('code-tutorial', ['ngMaterial', 'ui.ace'])
       var executedListener;
       var plugin = undefined;
       function execute() {
-        commandsArray.length = true;
-        for (var i = 0; i < instructions.length; i++) {
-          commandsArray[i] = {command: instructions[i], executed: false};
-        }
-        i = 0;
-        var interval = $interval(function() {
-          commandsArray[i].executed = true;
-          if (!actions[instructions[i++]]()) {
-            $interval.cancel(interval);
+        var result = false;
+        if (instructions.length) {
+          commandsArray.length = 0;
+          for (var i = 0; i < instructions.length; i++) {
+            commandsArray[i] = {command: instructions[i], executed: false};
           }
-        }, RobotTutorial.stepMilisecs, instructions.length);
-        var result = i === instructions.length;
-        if (result) {
-          alert("Congratulations! You have coped with the task!");
-        } else {
-          // for(i--; i >= 0; i--)
-          // {
-          //   rollbackActions[instructions[i]]();
-          // }
+          i = 0;
+          var interval = $interval(function() {
+            commandsArray[i].executed = true;
+            if (!actions[instructions[i++]]()) {
+              $interval.cancel(interval);
+            }
+          }, RobotTutorial.stepMilisecs, instructions.length);
+          result = i === instructions.length;
+          if (result) {
+            alert("Congratulations! You have coped with the task!");
+          } else {
+            // for(i--; i >= 0; i--)
+            // {
+            //   rollbackActions[instructions[i]]();
+            // }
+          }
         }
         if (executedListener) {
           executedListener(result);
@@ -316,6 +299,14 @@ angular.module('code-tutorial', ['ngMaterial', 'ui.ace'])
             orientationChangedListener = listener;
           }
         },
+        get onRobotMove() {
+          return positionChangedListener;
+        },
+        set onRobotMove(listener) {
+          if (!listener || typeof listener === 'function') {
+            positionChangedListener = listener;
+          }
+        },
         get onRobotOrientationChange() {
           return orientationChangedListener;
         },
@@ -355,11 +346,10 @@ angular.module('code-tutorial', ['ngMaterial', 'ui.ace'])
       LEFT: 'left'
     };
     RobotTutorial.genaralRobotClass = 'robot';
-    RobotTutorial.animationClasses = {
-      FromLeft: 'from-left',
-      FromRight: 'from-right',
-      EnterStart: 'enter',
-      EnterFinish: 'enter-active'
+    RobotTutorial.turnDirection = {
+      RIGHT: 1,
+      NONE: 0,
+      LEFT: -1
     };
     $scope.run = function() {
       engine.init(code);
@@ -367,10 +357,99 @@ angular.module('code-tutorial', ['ngMaterial', 'ui.ace'])
     $scope.board = [];
     $scope.commands = [];
     var engine = new RobotTutorial($scope.board, 5, [3, 3], [[0, 1], [3, 4], [4, 2], [3, 4]], $scope.commands);
-    engine.onRobotOrientationChange = function(newClass, childnumber, callback) {
+    engine.onRobotOrientationChange = function(newClass, direction, childnumber) {
       $scope.robotClass = newClass;
-      callback($('.robot-cell .robot'));
+      var cells = $('.robot-cell .robot');
+      if (!direction) {
+        return;
+      }
+      var oldDegree = cells.css("-webkit-transform") ||
+        cells.css("-moz-transform")    ||
+        cells.css("-ms-transform")     ||
+        cells.css("-o-transform")      ||
+        cells.css("transform");
+      oldDegree = parseInt(oldDegree.match(/-?[0-9]+deg/) ||
+        cells[0].style.transform.match(/-?[0-9]+deg/)) || 0;
+      if (direction > 0) {
+        var newDegree = oldDegree + 90;
+      } else {
+        newDegree = oldDegree - 90;
+      }
+      cells.css({
+        transform: 'rotate(' + newDegree + 'deg)'
+      });
+      // if artifact appears use solution below instead of cells.css + transition
+  
+      // $({deg: oldDegree}).animate({deg: newDegree}, {
+      //   duration: RobotTutorial.stepMilisecs,
+      //   step: function(now) {
+      //     cells.css({
+      //       transform: 'rotate(' + now + 'deg)'
+      //     });
+      //   }
+      // });
     };
+    engine.onRobotMove = function(orientation, finishAnimation) {
+      debugger;
+      var cells = $('.robot-cell .robot');
+      var cellsContainer = $('.robot-cell').parent();
+      var width = parseFloat(cells.css('width')) +
+        convertToPx(cellsContainer.attr('md-gutter'), cellsContainer.css('width'), cells) + 'px';
+      var height = parseFloat(cells.css('height')) +
+        convertToPx(cellsContainer.attr('md-gutter'), cellsContainer.css('height'), cells) + 'px';
+      var translate;
+      switch (orientation) {
+        case RobotTutorial.orientation.UP:
+          translate = '0, ' + height;
+          break;
+        case RobotTutorial.orientation.RIGHT:
+          translate = '-' + width + ', 0';
+          break;
+        case RobotTutorial.orientation.BOTTOM:
+          translate = '0, -' + height;
+          break;
+        case RobotTutorial.orientation.LEFT:
+          translate = width + ', 0';
+          break;
+      }
+      cells.css({
+        transform: '+=,translate(' + translate + ')',
+        transition: 'all ' + RobotTutorial.stepMilisecs + ' ease-in'
+      });
+      finishAnimation(function() {
+        var transform = cells.css('transform').replace(/translate\(-?[0-9]px[,\s*-?[0-9]px]\)/);
+        cells.css({
+          transform: transform
+        });
+      });
+    };
+    ///!!!! basis must be a number
+    function convertToPx(value, basis, element) {
+      basis = parseFloat(basis);
+      var units = value.match(/[%a-zA-Z]+$/);
+      value = parseFloat(value);
+      switch (units) {
+        case '%':
+          value *= 0.01 * basis;
+          break;
+        case 'rem':
+          value = convertRemToPixels(value);
+          break;
+        case 'em':
+          value = convertEmToPixels(value, element);
+          break;
+      }
+      return value;
+      function convertRemToPixels(rem) {
+        return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+      }
+      function convertEmToPixels(em, element) {
+        if (jQuery && element instanceof jQuery) {
+          element = element[0];
+        }
+        return em * parseFloat(getComputedStyle(element.parentNode).fontSize);
+      }
+    }
     $scope.commandsId = 'commands';
     engine.onInterpretEnd = function(ok, execute) {
       if (ok) {
